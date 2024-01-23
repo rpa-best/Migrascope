21536735c475e8ab247091cfa1f71727e643df56
Daaski <Redkin.Artem.2017@yandex.ru>
2024-01-23 11:55:58 +0300

    test

--- src/app/(Main)/components/ProgressBars/loading.tsx
+++
@@ -1,6 +0,0 @@
-import {BarsSkeleton} from "app/(Main)/components/Sceletons/Skeletons";
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <BarsSkeleton />
-}
--- src/components/News/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { NewsSkeleton } from 'components/News/Skeleton';
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <NewsSkeleton />;
-}
